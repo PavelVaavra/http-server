4551738b2360550c596c32a8b1fa4408d3c85644
PavelVaavra <pavelvavra.vm@gmail.com>
2025-09-15 18:35:43 +0200

    fileserver

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 
 	server := &http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: mux,
 	}
 
+	fmt.Printf("Serving files from %v on port: %v\n", filepathRoot, port)
 	server.ListenAndServe()
 }
