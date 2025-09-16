45b77305eccd353585a4935ced1d9694c6996ca9
PavelVaavra <pavelvavra.vm@gmail.com>
2025-09-16 22:24:30 +0200

    stateful handlers

--- main.go
+++ main.go
@@ -3,21 +3,48 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) middlewareMetricsPrint(w http.ResponseWriter, _ *http.Request) {
+	s := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+	w.Write([]byte(s))
+}
+
+func (cfg *apiConfig) middlewareMetricsReset(_ http.ResponseWriter, _ *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	apiCfg := apiConfig{}
+
 	mux := http.NewServeMux()
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
+
+	mux.HandleFunc("/metrics", apiCfg.middlewareMetricsPrint)
+
+	mux.HandleFunc("/reset", apiCfg.middlewareMetricsReset)
 
 	server := &http.Server{
 		Addr:    ":" + port,
